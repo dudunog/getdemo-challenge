487c2383b36a75f7f4b4982aa87b8563e2ccf498
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-08-25 19:07:38 -0300

    refactor: Update frames-api to handle query updates in demosApi

--- src/modules/demos/services/frames-api/frames-api.ts
+++ src/modules/demos/services/frames-api/frames-api.ts
@@ -1,6 +1,7 @@
 import { Frame } from '@/entities/Frame';
 import { baseURL } from '@/shared/constants/base-url';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { demosApi } from '../demos-api';
 
 export type FramesResponse = Frame[]
 
@@ -15,6 +16,24 @@ export const framesApi = createApi({
         method: 'PUT',
         body,
       }),
+      async onQueryStarted({ id, ...data }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          demosApi.util.updateQueryData('getDemos', undefined, draft => {
+            const demo = draft.find(demo => demo.id === id);
+            if (demo) {
+              const frame = demo.frames.find(frame => frame.id === id);
+              if (frame) {
+                Object.assign(frame, data);
+              }
+            }
+          })
+        );
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
       invalidatesTags: ['Frames'],
     }),
   }),
